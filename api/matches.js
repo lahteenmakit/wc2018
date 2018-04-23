8e070e14443e1c3fbab6fee98a60b41afa6a5f78
tuomas.lahteenmaki <tuomas.lahteenmaki@avarko.com>
2018-04-23 21:07:48 +0300

    Added participiants data table

--- api/matches.js
+++ api/matches.js
@@ -2,7 +2,6 @@ var Match = require('../models/Match');
 var app = require('../server');
 
 app.get('/matches/:id?', (req, res, next) => {
-    console.log('hello')
     if(req.params.id) {
       Match.getMatchById(req.params.id, (err, rows) => {
         if (err) {
---
+++ sql/insert-participiants.sql
@@ -0,0 +1,12 @@
+create table participiants (
+	email varchar(320),
+	username varchar(50),
+	password varchar(44),
+	points int
+);
+
+insert into participiants (email, username, password, points)
+values ('pekka@testi.com','pekka','salasana',0)
+values ('markku@testi.com','markku','salasana',0)
+values ('jussi@testi.com','jussi','salasana',0)
+values ('timo@testi.com','timo','salasana',0);
