a11207c8fa7d7d60dc3fd4d5e4a4e8d7375ef1ff
Tuomas Lähteenmäki <tuomaslahteenmaki@Tuomass-MacBook-Pro.local>
2018-05-22 19:43:11 +0300

    Minor improvements

--- handlebarsHelpers.js
+++ handlebarsHelpers.js
@@ -4,6 +4,25 @@ hbs.registerHelper('addOne', (value, options) => {
   return parseInt(value) + 1;
 });
 
+hbs.registerHelper('getRank', (value, options) => {
+	var suffix = '';
+	var number = parseInt(value) + 1;
+	switch(number) {
+		case 1:
+			suffix = 'st'
+			break;
+		case 2:
+			suffix = 'nd'
+			break;
+		case 3:
+			suffix = 'rd'
+			break;
+		default:
+			suffix = 'th'
+	}
+  	return number+suffix;
+});
+
 hbs.registerHelper('parseImgFileName', (value, options) => {
   return value.toLowerCase() + '.png';
 });
